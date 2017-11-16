59b73c0bac315e6841d3efd7c851690aea30c5e2
cokoyoh <charlesokoyoh@gmail.com>
2017-11-16 12:45:20 +0300

    initializing a todo list application

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,13 @@
 <template>
     <div id = "app" class = "app">
-        <input type = "text" id = "input" v-model = "message">
-        <p>The value of this input is {{message}}</p>
-
-        <div class = "interns">
-            <h4>The Interns Names are</h4>
-            <ul v-for="intern in interns.names">
-                <li>{{intern}}</li>
-            </ul>
-        </div>
-
-        <div class="conditionals">
-            <h4>Remarks | Vue Conditionals</h4>
-            <p v-if="marks.cokoyoh >= 60">Charles scored above average</p>
-            <p v-if="marks.shangri_la == 100">I know that place, That is Shangri-La. It's magical</p>
-        </div>
-
-        <div class="event-handling">
-            <button type="button" @click="onSubmitButton">Please Click Me</button>
-        </div>
+        <div class="button success">Foundation</div>
     </div>
 </template>
 
 <script>
     export default {
         data() {
-            return {
-                message: "Welcome to vue Charles",
-
-                interns: {
-                    names: ['charles', 'salome', 'the nyamu', 'caro', 'kabiru', 'douglas', 'maina', 'mwiti']
-                },
-
-                marks:{
-                    'cokoyoh': 78,
-                    'yoda': 99,
-                    'shangri_la': 100
-                }
-            }
-        },
-        methods:{
-            onSubmitButton(){
-                let what_now = "Learning Vue? You must be a genius!"
-                alert(what_now)
-                console.log("Mmh! ",what_now)
-            }
+            return {}
         }
     }
 </script>
