7dc3eb9b70ee7367b9fb350d6ee3df23c368d005
cokoyoh <charlesokoyoh@gmail.com>
2017-11-16 12:10:22 +0300

    Event handling added

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,13 @@
         </div>
 
         <div class="conditionals">
+            <h4>Remarks | Vue Conditionals</h4>
+            <p v-if="marks.cokoyoh >= 60">Charles scored above average</p>
+            <p v-if="marks.shangri_la == 100">I know that place, That is Shangri-La. It's magical</p>
+        </div>
 
+        <div class="event-handling">
+            <button type="button" @click="onSubmitButton">Please Click Me</button>
         </div>
     </div>
 </template>
@@ -29,8 +35,15 @@
                 marks:{
                     'cokoyoh': 78,
                     'yoda': 99,
-                    'shangri-La': 100
+                    'shangri_la': 100
                 }
+            }
+        },
+        methods:{
+            onSubmitButton(){
+                let what_now = "Learning Vue? You must be a genius!"
+                alert(what_now)
+                console.log("Mmh! ",what_now)
             }
         }
     }
